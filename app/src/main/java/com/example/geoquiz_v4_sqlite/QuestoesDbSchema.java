26692313e0d31bfe42d33a445c786ff6f8144f8d
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-10-03 13:46:28 -0300

    Criada classe RespostasTbl de esquema da tabela de resposta

--- app/src/main/java/com/example/geoquiz_v4_sqlite/QuestoesDbSchema.java
+++ app/src/main/java/com/example/geoquiz_v4_sqlite/QuestoesDbSchema.java
@@ -1,12 +1,23 @@
 package com.example.geoquiz_v4_sqlite;
 
 public class QuestoesDbSchema {
-    public static final class QuestoesTbl{
+
+    public static final class QuestoesTbl {
         public static final String NOME = "Questoes";
-        public static final class Cols{  //identificação das colunas da tabela Questões
+        public static final class Cols {
             public static final String UUID = "uuid";
             public static final String TEXTO_QUESTAO = "txt_questao";
             public static final String QUESTAO_CORRETA = "questao_correta";
+        }
+    }
+
+    public static final class RespostasTbl {
+        public static final String NOME = "Respostas";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String RESPOSTA_CORRETA = "resposta_correta";
+            public static final String RESPOSTA_OFERECIDA = "resposta_oferecida";
+            public static final String COLOU = "colou";
         }
     }
 }
