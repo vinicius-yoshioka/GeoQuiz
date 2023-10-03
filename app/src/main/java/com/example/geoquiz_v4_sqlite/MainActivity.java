94807643214a24407060b8a7c88573a2434f2efe
vinicius-yoshioka <101149767+vinicius-yoshioka@users.noreply.github.com>
2023-10-03 18:05:31 -0300

    Refatorado MainSctivity.java

--- app/src/main/java/com/example/geoquiz_v4_sqlite/MainActivity.java
+++ app/src/main/java/com/example/geoquiz_v4_sqlite/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.geoquiz_v4_sqlite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -12,46 +13,45 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/*
-  Modelo de projeto para a Atividade 1.
-  Será preciso adicionar o cadastro das respostas do usuário ao Quiz, conforme
-  definido no Canvas.
-
-  GitHub: https://github.com/udofritzke/GeoQuiz
- */
-
+/**
+ * Modelo de projeto para a Atividade 1.
+ * Será preciso adicionar o cadastro das respostas do usuário ao Quiz, conforme
+ * definido no Canvas.
+ * <p>
+ * GitHub: https://github.com/udofritzke/GeoQuiz
+ * */
 public class MainActivity extends AppCompatActivity {
-    private Button mBotaoVerdadeiro;
-    private Button mBotaoFalso;
-    private Button mBotaoProximo;
-    private Button mBotaoMostra;
-    private Button mBotaoDeleta;
 
-    private Button mBotaoCola;
 
-    private TextView mTextViewQuestao;
-    private TextView mTextViewQuestoesArmazenadas;
-
-    private static final String TAG = "QuizActivity";
+    private static final String TAG = "MainActivity";
     private static final String CHAVE_INDICE = "INDICE";
     private static final int CODIGO_REQUISICAO_COLA = 0;
 
-    private Questao[] mBancoDeQuestoes = new Questao[]{
+
+    private TextView mTextViewQuestao;
+    private Button mBotaoVerdadeiro;
+    private Button mBotaoFalso;
+    private Button mBotaoCola;
+    private Button mBotaoProximo;
+    private Button mBotaoMostra;
+    private Button mBotaoDeleta;
+    private TextView mTextViewQuestoesArmazenadas;
+
+    private QuestaoDB mQuestoesDb;
+    private Questao[] mBancoDeQuestoes = new Questao[] {
             new Questao(R.string.questao_suez, true),
             new Questao(R.string.questao_alemanha, false)
     };
-
-    QuestaoDB mQuestoesDb;
-
+    private boolean mEhColador;
     private int mIndiceAtual = 0;
 
-    private boolean mEhColador;
 
     @Override
     protected void onCreate(Bundle instanciaSalva) {
         super.onCreate(instanciaSalva);
         setContentView(R.layout.activity_main);
-        //Log.d(TAG, "onCreate()");
+        Log.d(TAG, "onCreate()");
+
         if (instanciaSalva != null) {
             mIndiceAtual = instanciaSalva.getInt(CHAVE_INDICE, 0);
         }
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         atualizaQuestao();
 
         mBotaoVerdadeiro = (Button) findViewById(R.id.botao_verdadeiro);
-        // utilização de classe anônima interna
         mBotaoVerdadeiro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -75,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 verificaResposta(false);
             }
         });
+
         mBotaoProximo = (Button) findViewById(R.id.botao_proximo);
         mBotaoProximo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,11 +89,8 @@ public class MainActivity extends AppCompatActivity {
         mBotaoCola.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // inicia ColaActivity
-                // Intent intent = new Intent(MainActivity.this, ColaActivity.class);
                 boolean respostaEVerdadeira = mBancoDeQuestoes[mIndiceAtual].isRespostaCorreta();
                 Intent intent = ColaActivity.novoIntent(MainActivity.this, respostaEVerdadeira);
-                //startActivity(intent);
                 startActivityForResult(intent, CODIGO_REQUISICAO_COLA);
             }
         });
@@ -115,50 +112,50 @@ public class MainActivity extends AppCompatActivity {
         //});
         //Cursor cur = mQuestoesDb.queryQuestao ("_id = ?", val);////(null, null);
         //String [] val = {"1"};
+
         mBotaoMostra = (Button) findViewById(R.id.botao_mostra_questoes);
         mBotaoMostra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                  Acesso ao SQLite
-                */
-                if (mQuestoesDb == null) return;
+                if (mQuestoesDb == null) {
+                    return;
+                }
+
                 if (mTextViewQuestoesArmazenadas == null) {
                     mTextViewQuestoesArmazenadas = (TextView) findViewById(R.id.texto_questoes_a_apresentar);
                 } else {
                     mTextViewQuestoesArmazenadas.setText("");
                 }
+
                 Cursor cursor = mQuestoesDb.queryQuestao(null, null);
                 if (cursor != null) {
                     if (cursor.getCount() == 0) {
                         mTextViewQuestoesArmazenadas.setText("Nada a apresentar");
                         Log.i("MSGS", "Nenhum resultado");
                     }
-                    //Log.i("MSGS", Integer.toString(cursor.getCount()));
-                    //Log.i("MSGS", "cursor não nulo!");
+
                     try {
                         cursor.moveToFirst();
                         while (!cursor.isAfterLast()) {
                             String texto = cursor.getString(cursor.getColumnIndex(QuestoesDbSchema.QuestoesTbl.Cols.TEXTO_QUESTAO));
                             Log.i("MSGS", texto);
 
-                            mTextViewQuestoesArmazenadas.append(texto + "\n");
+                            mTextViewQuestoesArmazenadas.append(texto + "\n\n");
                             cursor.moveToNext();
                         }
                     } finally {
                         cursor.close();
                     }
-                } else
+                } else {
                     Log.i("MSGS", "cursor nulo!");
+                }
             }
         });
+
         mBotaoDeleta = (Button) findViewById(R.id.botao_deleta);
         mBotaoDeleta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                  Acesso ao SQLite
-                */
                 if (mQuestoesDb != null) {
                     mQuestoesDb.removeBanco();
                     if (mTextViewQuestoesArmazenadas == null) {
@@ -168,8 +165,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
+
 
     private void atualizaQuestao() {
         int questao = mBancoDeQuestoes[mIndiceAtual].getTextoRespostaId();
@@ -185,14 +182,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             if (respostaPressionada == respostaCorreta) {
                 idMensagemResposta = R.string.toast_correto;
-            } else
+            } else {
                 idMensagemResposta = R.string.toast_incorreto;
+            }
         }
         Toast.makeText(this, idMensagemResposta, Toast.LENGTH_SHORT).show();
     }
 
+
     @Override
-    public void onSaveInstanceState(Bundle instanciaSalva) {
+    public void onSaveInstanceState(@NonNull Bundle instanciaSalva) {
         super.onSaveInstanceState(instanciaSalva);
         Log.i(TAG, "onSaveInstanceState()");
         instanciaSalva.putInt(CHAVE_INDICE, mIndiceAtual);
@@ -200,6 +199,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int codigoRequisicao, int codigoResultado, Intent dados) {
+        super.onActivityResult(codigoRequisicao, codigoResultado, dados);
         if (codigoResultado != Activity.RESULT_OK) {
             return;
         }
